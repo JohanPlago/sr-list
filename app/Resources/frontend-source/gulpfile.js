f03140ff82d701ed553c756622e7c9c0816625a2
Johan Palmfjord <johan.plago@gmail.com>
2016-02-08 21:17:40 +0100

    Move frontend assets source to app/Resources

--- app/Resources/frontend-source/gulpfile.js
+++ app/Resources/frontend-source/gulpfile.js
@@ -4,28 +4,29 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    webAssetsDir = '../../../web/assets';
 
 gulp.task('css', function() {
     return sass('source/scss/main.scss', { style: 'expanded' })
         .pipe(autoprefixer('last 2 version'))
         .pipe(gulp.dest('build/css'))
-        .pipe(gulp.dest('../web/assets/css'))
+        .pipe(gulp.dest(webAssetsDir + '/css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest('build/css'))
-        .pipe(gulp.dest('../web/assets/css'));
+        .pipe(gulp.dest(webAssetsDir + '/css'));
 });
 
 gulp.task('js', function() {
     return gulp.src('source/js/enabled/**/*.js')
         .pipe(concat('app.js'))
         .pipe(gulp.dest('build/js'))
-        .pipe(gulp.dest('../web/assets/js'))
+        .pipe(gulp.dest(webAssetsDir + '/js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('build/js'))
-        .pipe(gulp.dest('../web/assets/js'));
+        .pipe(gulp.dest(webAssetsDir + '/js'));
 });
 
 gulp.task('watch', function() {
