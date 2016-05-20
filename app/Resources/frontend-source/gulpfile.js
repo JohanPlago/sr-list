17e43af0050c5db5be5453b89a355d1dc676f628
Johan Palmfjord <johan.plago@gmail.com>
2016-05-20 19:25:12 +0200

    Remove redundant frontend build files

--- app/Resources/frontend-source/gulpfile.js
+++ app/Resources/frontend-source/gulpfile.js
@@ -10,22 +10,18 @@ var gulp = require('gulp'),
 gulp.task('css', function() {
     return sass('source/scss/main.scss', { style: 'expanded' })
         .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('build/css'))
         .pipe(gulp.dest(webAssetsDir + '/css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest('build/css'))
         .pipe(gulp.dest(webAssetsDir + '/css'));
 });
 
 gulp.task('js', function() {
     return gulp.src(['source/js/app.js', 'source/js/enabled/**/*.js'])
         .pipe(concat('app.js'))
-        .pipe(gulp.dest('build/js'))
         .pipe(gulp.dest(webAssetsDir + '/js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('build/js'))
         .pipe(gulp.dest(webAssetsDir + '/js'));
 });
 
