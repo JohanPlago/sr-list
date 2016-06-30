c5e67b2ffabf864f646069c7dbd0c734f93206e3
Johan Palmfjord <johan.plago@gmail.com>
2016-06-30 14:34:03 +0000

    Add an entry point that returns 401 instead of redirect on unauth. ajax calls

--- src/AppBundle/Security/Http/EntryPoint/AjaxEntryPoint.php
+++ src/AppBundle/Security/Http/EntryPoint/AjaxEntryPoint.php
@@ -9,12 +9,12 @@
 
 namespace AppBundle\Security\Http\EntryPoint;
 
+use HWI\Bundle\OAuthBundle\Security\Http\EntryPoint\OAuthEntryPoint;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\EntryPoint\FormAuthenticationEntryPoint;
 
-class AjaxEntryPoint extends FormAuthenticationEntryPoint
+class AjaxEntryPoint extends OAuthEntryPoint
 {
     public function start(Request $request, AuthenticationException $authException = null)
     {
